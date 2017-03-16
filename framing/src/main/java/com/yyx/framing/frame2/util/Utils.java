092915fcfd47ed35bdf17adac152309e84963e5c
vimi8 <466824141>
2017-03-16 14:21:17 +0800

    yyx

---
+++ framing/src/main/java/com/yyx/framing/frame2/util/Utils.java
@@ -0,0 +1,12 @@
+package com.yyx.framing.frame2.util;
+
+import android.content.Context;
+
+public class Utils {
+
+    public static int convertDpToPixel(Context context, int dp) {
+        float density = context.getResources().getDisplayMetrics().density;
+        return Math.round((float) dp * density);
+    }
+
+}
